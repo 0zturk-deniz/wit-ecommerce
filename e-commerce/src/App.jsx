931c139d380467ec6d8a1c7c865cf9fe2c7698be
0zturk-deniz <ddeniz.ozturkk@gmail.com>
2025-07-02 22:35:19 +0300

    first commit - creates tailwind, react, router, redux, axios, lucide react and toastify

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-red-500 text-white text-center text-4xl">
+        Tailwind artık %100 çalışıyor! 🔥
+      </div>
+    </>
+  );
+}
+
+export default App;
